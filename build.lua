881a13cde47e88995a12a5b77de98abdb7476371
Philip Kime <Philip@kime.org.uk>
2014-11-23 19:32:53 +0100

    Only test with luatex

--- build.lua
+++ build.lua
@@ -4,6 +4,7 @@ module = "biblatex"
 -- variable overwrites (if needed)
 typesetexe = "lualatex"
 stdengine = "luatex"
+checkengines = {"luatex"}
 -- call standard script
 kpse.set_program_name ("kpsewhich")
 dofile (kpse.lookup ("l3build.lua"))
