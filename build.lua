cff5e48d516541d71c0e9f89d245f48e254b0889
Philip Kime <Philip@kime.org.uk>
2015-11-03 12:09:37 +0100

    Some new beta test files and changes the default alphatemplate to use new prefix options

--- build.lua
+++ build.lua
@@ -2,10 +2,9 @@
 -- Build script for biblatex package
 module = "biblatex"
 -- variable overwrites (if needed)
-typesetexe = "lualatex"
 stdengine = "luatex"
 checkengines = {"luatex"}
-checksuppfiles = {"*.tex"}
 -- call standard script
 kpse.set_program_name ("kpsewhich")
 dofile (kpse.lookup ("l3build.lua"))
+
