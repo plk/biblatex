181c545efa6506896c1602a93629d881f95f61ab
Philip Kime <Philip@kime.org.uk>
2015-11-09 22:25:31 +0100

    l3build basic test working

--- build.lua
+++ build.lua
@@ -4,6 +4,9 @@ module = "biblatex"
 -- variable overwrites (if needed)
 stdengine = "luatex"
 checkengines = {"luatex"}
+checkruns    = 2
+checksuppfiles = {"bltx-test.tex"}
+testsuppdir    = "testfiles/helpers"
 -- call standard script
 kpse.set_program_name ("kpsewhich")
 dofile (kpse.lookup ("l3build.lua"))
