c90b12b0000eabf0f1cdb7a378813275b4e8a5ef
Joseph Wright <joseph.wright@morningstar2.co.uk>
2016-05-22 22:21:59 +0100

    Trying out some new l3build stuff
    We've now got something working on PDF comparison, though at
    present it's a work-in-progress.

--- build.lua
+++ build.lua
@@ -5,8 +5,11 @@ module = "biblatex"
 stdengine = "luatex"
 checkengines = {"luatex"}
 checkruns    = 2
-checksuppfiles = {"bltx-test.tex"}
-testsuppdir    = "testfiles/helpers"
+
+function runtest_tasks(name)
+  return "biber -q " .. name 
+end
+
 -- call standard script
 kpse.set_program_name ("kpsewhich")
 dofile (kpse.lookup ("l3build.lua"))
