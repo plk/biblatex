92a33ea639c8bc56785d122e14e4ed8095b31e78
Joseph Wright <joseph.wright@morningstar2.co.uk>
2022-02-22 18:02:01 +0000

    Setup to allow building documentation and most of release
    At present I've not covered the biber tree at the top level:
    need more info there.

--- build.lua
+++ build.lua
@@ -1,16 +1,27 @@
-#!/usr/bin/env texlua
 -- Build script for biblatex package
+
 module = "biblatex"
--- variable overwrites (if needed)
-stdengine = "luatex"
+
+-- TDS-based installation
+installfiles = {}
+sourcefiles = {}
+unpackfiles = {}
+tdsdirs = {bibtex = "bibtex", tex = "tex"}
+
+-- For the manual
+docfiledir = "./doc/latex/biblatex"
+docfiles = {"**/*.pdf", "**/*.tex"}
+typesetexe = "lualatex"
+typesetfiles = {"biblatex.tex"}
+
+flatten = false
+flattentds = false
+packtdszip = true
+
 checkengines = {"luatex"}
-checkruns    = 2
+checkruns = 2
 
 function runtest_tasks(name)
-  return "biber -q " .. name 
+  return "biber -q " .. name
 end
-
--- call standard script
-kpse.set_program_name ("kpsewhich")
-dofile (kpse.lookup ("l3build.lua"))
 
