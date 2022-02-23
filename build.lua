f0c50e976f535ea05e6678946b06a7551f7872ce
Joseph Wright <joseph.wright@morningstar2.co.uk>
2022-02-23 08:15:21 +0000

    Auto-tag biblatex.sty during testing
    A new feature will be needed in l3build to cover
    installation/typesetting, but for the present this
    should get us moving.

--- build.lua
+++ build.lua
@@ -12,10 +12,40 @@ function update_tag(file,content,tagname,tagdate)
 end
 
 -- TDS-based installation
-installfiles = {}
-sourcefiles = {}
+installfiles = {"./tex/latex/biblatex/biblatex.sty"}
+sourcefiles = installfiles
 unpackfiles = {}
 tdsdirs = {bibtex = "bibtex", tex = "tex"}
+
+-- For auto-editing
+local tagname = "v3.17"
+local tagdate = os.date("%Y-%m-%d")
+
+function checkinit_hook()
+  local file = unpackdir .. "/biblatex.sty"
+  local filename = basename(file)
+  local f = assert(io.open(file,"rb"))
+  local content = f:read("*all")
+  f:close()
+  -- Deal with Unix/Windows line endings
+  content = string.gsub(content .. (string.match(content,"\n$") and "" or "\n"),
+    "\r\n", "\n")
+  local updated_content = update_tag(filename,content,tagname,tagdate)
+  if content == updated_content then
+    return 0
+  else
+    local path = dirname(file)
+    ren(path,filename,filename .. ".bak")
+    f = assert(io.open(file,"w"))
+    -- Convert line ends back if required during write
+    -- Watch for the second return value!
+    f:write((string.gsub(updated_content,"\n",os_newline)))
+    f:close()
+    rm(path,filename .. ".bak")
+  end
+  cp("biblatex.sty",unpackdir,testdir)
+  return 0
+end
 
 -- For the manual
 docfiledir = "./doc/latex/biblatex"
